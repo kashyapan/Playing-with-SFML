1df4b4322a6b621aae3cd00fa8aad915ec0d4ab2
Kashyap <kashyapan4@gmail.com>
2017-06-24 16:18:42 +0530

    using namespace sf

---
+++ 2-Getting rid of sf/test.cpp
@@ -0,0 +1,25 @@
+#include <SFML/Graphics.hpp>
+using namespace sf ;
+//This is not the best practice but for learning it will do
+int main()
+{
+    RenderWindow window(VideoMode(200, 200), "SFML works!");
+    CircleShape shape(100.f);
+    shape.setFillColor(Color::Blue);
+
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
