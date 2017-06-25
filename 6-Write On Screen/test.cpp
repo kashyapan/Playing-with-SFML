93f41ebbf5498aae590f7231cb789d2eab44fa6b
Kashyap <kashyapan4@gmail.com>
2017-06-25 13:03:47 +0530

    Just Writes Esc to Close

---
+++ 6-Write On Screen/test.cpp
@@ -0,0 +1,89 @@
+
+#include<SFML/Graphics.hpp>
+#include<iostream>
+#include<string>
+
+using namespace sf ;
+using namespace std; 
+
+
+int main()
+{
+	RenderWindow window(VideoMode(800,600) , "Write here Randomly" , Style::Default) ;
+	
+	string displayText = " ";
+	
+	Text text ;
+	Font sans ;
+	
+	sans.loadFromFile("./fonts/SFCartoonist.ttf") ;
+
+	text.setFont(sans);	
+	text.setCharacterSize(24) ;
+	text.setColor(Color::Green) ;
+	text.setString(displayText);					
+	
+	while(window.isOpen())
+	{
+
+		Event e ;
+		
+		//Keep looking for events to occur.
+		//Event Loop!
+		while(window.pollEvent(e))
+		{
+			//Detect for pressed keys
+			if(e.type == Event::KeyPressed)
+			{
+
+				//Exit if Esc is pressed
+				if(e.key.code == Keyboard::Escape)
+				{
+					window.close() ;
+				}
+								
+				//Delete Character by reducing str len
+				if(e.key.code == Keyboard::BackSpace)
+				{
+					displayText.erase(displayText.end()-1)  ;								
+					
+				}	
+				
+				//Detect Enter Key and add a new line
+				if(e.key.code == Keyboard::Return)
+				{
+					displayText += '\n' ;			
+				}
+				
+			}
+
+			//Check if printable text is Pressed
+				
+			if( e.type == Event::TextEntered )
+			{
+				
+				if(e.text.unicode < 128 && e.text.unicode != 8 ){
+					displayText = displayText + static_cast<char>(e.text.unicode) ;
+//					cout <<static_cast<char>(e.text.unicode)<<" Added" <<endl ;						
+					
+				}
+
+			}
+
+
+	
+		}
+
+		
+	text.setString(displayText);
+
+	window.clear();
+	window.draw(text) ;
+	window.display();		
+
+	
+	}
+		
+	
+	return 0 ;
+}
